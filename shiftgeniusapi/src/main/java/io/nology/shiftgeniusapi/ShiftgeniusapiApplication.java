eb96b91ed0caba43be924bdaf693ead647725c51
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2024-10-10 12:16:37 +1100

    scaffold app

---
+++ shiftgeniusapi/src/main/java/io/nology/shiftgeniusapi/ShiftgeniusapiApplication.java
@@ -0,0 +1,13 @@
+package io.nology.shiftgeniusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftgeniusapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftgeniusapiApplication.class, args);
+	}
+
+}
