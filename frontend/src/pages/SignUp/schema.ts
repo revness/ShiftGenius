421c697a6a819c834f0d66112a2c9a41d1d21f26
Xiao Zhong <zhongxiao2017@gmail.com>
2024-10-14 09:52:06 +1100

    signin signup ui

---
+++ frontend/src/pages/SignUp/schema.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+
+const schema = z
+  .object({
+    username: z.string().min(3, "Username must be at least 3 characters"),
+    email: z.string().email("Email must be a valid email address"),
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .regex(/[A-Z]+/, "Password must contain a capital letter")
+      .regex(/[0-9]+/, "Password must contain a number"),
+    passwordConfirm: z.string(),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "Password confirm must match password",
+    path: ["passwordConfirm"],
+  });
+
+export default schema;
+export type SignUpInput = z.infer<typeof schema>;
