efa208ed8596e9c14823674c3dac3330100770af
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2024-10-21 12:31:46 +1100

    sets up config for cors

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import Calendar from "../../components/Calendar/Calendar";
+import { getTimeSheet } from "../../services/shift";
 
 const Dashboard = () => {
+  useEffect(() => {
+    console.log(
+      "get timesheet",
+      getTimeSheet("2007-12-03").then((res) => console.log(res))
+    );
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <Calendar />
--- frontend/src/services/shift.ts
+++ frontend/src/services/shift.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { SignUpFormInputs } from "../pages/SignUp/SignUp";
 import { SignInFormInputs } from "../pages/SignIn/SignIn";
+import { ShiftFormInputs } from "../components/Shifts/Shifts";
 
 const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
 
@@ -37,5 +38,39 @@ export const logIn = async (data: SignInFormInputs) => {
     return response.data as User;
   } catch (error) {
     throw new Error("Failed to login");
+  }
+};
+
+export const postTimeSheet = async (data: ShiftFormInputs) => {
+  try {
+    console.log("Posting to URL:", `${baseURL}/timesheet`);
+
+    const response = await axios.post(`${baseURL}/timesheet`, data, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huc21pdGhAZ21haWwuY29tIiwiaWF0IjoxNzI5NDU4NDkzLCJleHAiOjE3Mjk1NDQ4OTN9.esI-cbMfHlVXmp_cFjjqaZsxOxtdbVfEEPxI5qom-yI`,
+      },
+    });
+    console.log("timesheet", response.data);
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to post timesheet");
+  }
+};
+
+export const getTimeSheet = async (date: String) => {
+  try {
+    const response = await axios.get(`${baseURL}/timesheet/`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huQGdtYWlsLmNvbSIsImlhdCI6MTcyOTQ2NTc3NiwiZXhwIjoxNzI5NTUyMTc2fQ.6ZT-5y_pA8oAfO04U_Siti_EY0QoUJMwSNKn_aVnpQA",
+      },
+      params: { date: date },
+    });
+    console.log("timesheet", response.data);
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to get timesheet");
   }
 };
