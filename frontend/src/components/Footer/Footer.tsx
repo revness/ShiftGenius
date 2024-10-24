d92e261aa319819ca187129109f7ce834c85f676
Xiao Zhong <zhongxiao2017@gmail.com>
2024-10-24 12:43:51 +1100

    update homepage

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex justify-center mt-8 font-cambria">
+    <div className="flex justify-center mt-4 font-cambria">
       <p> &copy; 2024 Shift Genius. Designed by Chapman & Jenny</p>
     </div>
   );
